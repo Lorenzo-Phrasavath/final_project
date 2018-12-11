ad5c352b7a51f4f67c584cba1bc000e5690a0299
Lorenzo Phrasavath <lorenzophrasavath@gmail.com>
2018-12-10 19:02:51 -0500

    final commit

---
+++ js/audio.js
@@ -0,0 +1,318 @@
+/*Audio Player design derived from Alex Katz at http://alexkatz.me/posts/building-a-custom-html5-audio-player-with-javascript/*/
+
+$(document).ready(function() {
+
+let music1 = document.getElementById('music1');
+let music2 = document.getElementById('music2');
+let music3 = document.getElementById('music3');
+let music4 = document.getElementById('music4');
+let music5 = document.getElementById('music5');
+let music6 = document.getElementById('music6');
+let music7 = document.getElementById('music7');
+let music8 = document.getElementById('music8');
+let music9 = document.getElementById('music9');
+let music10 = document.getElementById('music10');
+let music11 = document.getElementById('music11');
+let music12 = document.getElementById('music12');
+let music13 = document.getElementById('music13');
+let music14 = document.getElementById('music14');
+let music15 = document.getElementById('music15');
+let music16 = document.getElementById('music16');
+let music17 = document.getElementById('music17');
+let music18 = document.getElementById('music18');
+
+function playAudio1() {
+	if (music1.paused) {
+		music1.play();
+		pButton1.className = "";
+		pButton1.className = "pause";
+    icon1.src = "images/pause.png";
+	} else {
+		music1.pause();
+		pButton1.className = "";
+		pButton1.className = "play";
+    icon1.src = "images/play.png";
+	}
+}
+function playAudio2() {
+	if (music2.paused) {
+		music2.play();
+		pButton2.className = "";
+		pButton2.className = "pause";
+    icon2.src = "images/pause.png";
+	} else {
+		music2.pause();
+		pButton2.className = "";
+		pButton2.className = "play";
+    icon2.src = "images/play.png";
+	}
+}
+function playAudio3() {
+	if (music3.paused) {
+		music3.play();
+		pButton3.className = "";
+		pButton3.className = "pause";
+    icon3.src = "images/pause.png";
+	} else {
+		music3.pause();
+		pButton3.className = "";
+		pButton3.className = "play";
+    icon3.src = "images/play.png";
+	}
+}
+function playAudio4() {
+	if (music4.paused) {
+		music4.play();
+		pButton4.className = "";
+		pButton4.className = "pause";
+    icon4.src = "images/pause.png";
+	} else {
+		music4.pause();
+		pButton4.className = "";
+		pButton4.className = "play";
+    icon4.src = "images/play.png";
+	}
+}
+function playAudio5() {
+	if (music5.paused) {
+		music5.play();
+		pButton5.className = "";
+		pButton5.className = "pause";
+    icon5.src = "images/pause.png";
+	} else {
+		music5.pause();
+		pButton5.className = "";
+		pButton5.className = "play";
+    icon5.src = "images/play.png";
+	}
+}
+function playAudio6() {
+	if (music6.paused) {
+		music6.play();
+		pButton6.className = "";
+		pButton6.className = "pause";
+    icon6.src = "images/pause.png";
+	} else {
+		music6.pause();
+		pButton6.className = "";
+		pButton6.className = "play";
+    icon6.src = "images/play.png";
+	}
+}
+function playAudio7() {
+	if (music7.paused) {
+		music7.play();
+		pButton7.className = "";
+		pButton7.className = "pause";
+    icon7.src = "images/pause.png";
+	} else {
+		music7.pause();
+		pButton7.className = "";
+		pButton7.className = "play";
+    icon7.src = "images/play.png";
+	}
+}
+function playAudio8() {
+	if (music8.paused) {
+		music8.play();
+		pButton8.className = "";
+		pButton8.className = "pause";
+    icon8.src = "images/pause.png";
+	} else {
+		music8.pause();
+		pButton8.className = "";
+		pButton8.className = "play";
+    icon8.src = "images/play.png";
+	}
+}
+function playAudio9() {
+	if (music9.paused) {
+		music9.play();
+		pButton9.className = "";
+		pButton9.className = "pause";
+    icon9.src = "images/pause.png";
+	} else {
+		music9.pause();
+		pButton9.className = "";
+		pButton9.className = "play";
+    icon9.src = "images/play.png";
+	}
+}
+function playAudio10() {
+	if (music10.paused) {
+		music10.play();
+		pButton10.className = "";
+		pButton10.className = "pause";
+    icon10.src = "images/pause.png";
+	} else {
+		music10.pause();
+		pButton10.className = "";
+		pButton10.className = "play";
+    icon10.src = "images/play.png";
+	}
+}
+function playAudio11() {
+	if (music11.paused) {
+		music11.play();
+		pButton11.className = "";
+		pButton11.className = "pause";
+    icon11.src = "images/pause.png";
+	} else {
+		music11.pause();
+		pButton11.className = "";
+		pButton11.className = "play";
+    icon11.src = "images/play.png";
+	}
+}
+function playAudio12() {
+	if (music12.paused) {
+		music12.play();
+		pButton12.className = "";
+		pButton12.className = "pause";
+    icon12.src = "images/pause.png";
+	} else {
+		music12.pause();
+		pButton12.className = "";
+		pButton12.className = "play";
+    icon12.src = "images/play.png";
+	}
+}
+function playAudio13() {
+	if (music13.paused) {
+		music13.play();
+		pButton13.className = "";
+		pButton13.className = "pause";
+    icon13.src = "images/pause.png";
+	} else {
+		music13.pause();
+		pButton13.className = "";
+		pButton13.className = "play";
+    icon13.src = "images/play.png";
+	}
+}
+function playAudio14() {
+	if (music14.paused) {
+		music14.play();
+		pButton14.className = "";
+		pButton14.className = "pause";
+    icon14.src = "images/pause.png";
+	} else {
+		music14.pause();
+		pButton14.className = "";
+		pButton14.className = "play";
+    icon14.src = "images/play.png";
+	}
+}
+function playAudio15() {
+	if (music15.paused) {
+		music15.play();
+		pButton15.className = "";
+		pButton15.className = "pause";
+    icon15.src = "images/pause.png";
+	} else {
+		music15.pause();
+		pButton15.className = "";
+		pButton15.className = "play";
+    icon15.src = "images/play.png";
+	}
+}
+function playAudio16() {
+	if (music16.paused) {
+		music16.play();
+		pButton16.className = "";
+		pButton16.className = "pause";
+    icon16.src = "images/pause.png";
+	} else {
+		music16.pause();
+		pButton16.className = "";
+		pButton16.className = "play";
+    icon16.src = "images/play.png";
+	}
+}
+function playAudio17() {
+	if (music17.paused) {
+		music17.play();
+		pButton17.className = "";
+		pButton17.className = "pause";
+    icon17.src = "images/pause.png";
+	} else {
+		music17.pause();
+		pButton17.className = "";
+		pButton17.className = "play";
+    icon17.src = "images/play.png";
+	}
+}
+function playAudio18() {
+	if (music18.paused) {
+		music18.play();
+		pButton18.className = "";
+		pButton18.className = "pause";
+    icon18.src = "images/pause.png";
+	} else {
+		music18.pause();
+		pButton18.className = "";
+		pButton18.className = "play";
+    icon18.src = "images/play.png";
+	}
+}
+
+
+$('.icon').click(function() {
+switch ($(this).attr("id")) {
+    case "icon1":
+      playAudio1();
+      break;
+    case "icon2":
+      playAudio2();
+      break;
+    case "icon3":
+      playAudio3();
+      break;
+    case "icon4":
+      playAudio4();
+      break;
+    case "icon5":
+      playAudio5();
+      break;
+    case "icon6":
+      playAudio6();
+      break;
+    case "icon7":
+      playAudio7();
+      break;
+    case "icon8":
+      playAudio8();
+      break;
+    case "icon9":
+      playAudio9();
+      break;
+    case "icon10":
+      playAudio10();
+      break;
+    case "icon11":
+      playAudio11();
+      break;
+    case "icon12":
+      playAudio12();
+      break;
+    case "icon13":
+      playAudio13();
+      break;
+    case "icon14":
+      playAudio14();
+      break;
+    case "icon15":
+      playAudio15();
+      break;
+    case "icon16":
+      playAudio16();
+      break;
+    case "icon17":
+      playAudio17();
+      break;
+    case "icon18":
+      playAudio18();
+      break;
+}
+});
+});
--- js/main.js
+++ js/main.js
@@ -1,60 +1,217 @@
 $(document).ready(function() {
 
-  /*  $(window).on('load', function() {
-      $('#div3').animate({top: '+=25px'}, "slow");
-    }); */
-
     function resetAll () {
         $('.text').hide();
-        $('.info').animate({width: '50px'}, "fast");
-        $('.info').animate({height: '400px'}, "fast");
-        $('.info').css('background-color', '#99A3A4');
+        $('.shape').animate({width: '50px'}, 200);
+        $('.shape').animate({height: '400px'}, 200);
+        $('.shape').css('background-color', '#FFF');
+        $('audio').each(function() {
+          this.pause();
+          this.currentTime = 0;
+        });
+        $('.audio').hide();
         $('.hide').hide();
-        $('.show').show();
+        $('.piano-keys').delay(400).show();
     };
 
-    $('#show1').click(function(){
+    function hideMusic() {
+        $('#original_dixieland-audio').hide();
+        $('#louis_armstrong-audio').hide();
+        $('#benny_goodman-audio').hide();
+        $('#glenn_miller-audio').hide();
+        $('#dizzy_gillespie-audio').hide();
+        $('#art_blakey-audio').hide();
+        $('#miles_davis-audio').hide();
+        $('#chet_baker-audio').hide();
+        $('#john_coltrane-audio').hide();
+        $('#charles_mingus-audio').hide();
+        $('#miles_davis2-audio').hide();
+        $('#weather_report-audio').hide();
+        $('#mario_bauza-audio').hide();
+        $('#stan_getz-audio').hide();
+        $('#grover_washington-audio').hide();
+        $('#kenny_g-audio').hide();
+        $('#jamiroquai-audio').hide();
+        $('#us3-audio').hide();
+    };
+
+    function pause () {
+      $('audio').each(function() {
+        this.pause();
+        this.currentTime = 0;
+      $('.icon').attr('src', 'images/play.png');
+      });
+    };
+
+    $('#dixieland-key').click(function(){
         resetAll();
-        $('#div1').css('background-color', '#EC7063');
-        $('#show1').hide();
+        $('#dixieland-key').hide();
+        $('#dixieland').css('background-color', '#F5B041');
         $('#hide1').show();
-        $('#div1').animate({height: '500px'}, 400);
-        $('#div1').animate({width: '400px'}, 400);
-        $('#text1').delay(1100).slideDown();
+        $('#dixieland').animate({height: '510px'}, 400);
+        $('#dixieland').animate({width: '400px'}, 400);
+        $('#dixieland-text').delay(1100).slideDown();
     });
-
-    $('#show2').click(function(){
+    $('#swing-key').click(function(){
         resetAll();
-        $('#div2').css('background-color', '#58D68D');
-        $('#show2').hide();
+        $('#swing-key').hide();
+        $('#swing').css('background-color', '#F4D03F');
         $('#hide2').show();
-        $('#div2').animate({height: '500px'}, 400);
-        $('#div2').animate({width: '400px'}, 400);
-        $('#text2').delay(1100).slideDown();
+        $('#swing').animate({height: '510px'}, 400);
+        $('#swing').animate({width: '400px'}, 400);
+        $('#swing-text').delay(1100).slideDown();
     });
-
-    $('#show3').click(function(){
+    $('#bebop-key').click(function(){
         resetAll();
-        $('#div3').css('background-color', '#5DADE2');
-        $('#show3').hide();
+        $('#bebop-key').hide();
+        $('#bebop').css('background-color', '#EC7063');
         $('#hide3').show();
-        $('#div3').animate({height: '500px'}, 400);
-        $('#div3').animate({width: '400px'}, 400);
-        $('#text3').delay(1100).slideDown();
+        $('#bebop').animate({height: '510px'}, 400);
+        $('#bebop').animate({width: '400px'}, 400);
+        $('#bebop-text').delay(1100).slideDown();
+    });
+    $('#cool-key').click(function(){
+        resetAll();
+        $('#cool-key').hide();
+        $('#cool').css('background-color', '#5DADE2');
+        $('#hide4').show();
+        $('#cool').animate({height: '510px'}, 400);
+        $('#cool').animate({width: '400px'}, 400);
+        $('#cool-text').delay(1100).slideDown();
+    });
+    $('#free-key').click(function(){
+        resetAll();
+        $('#free-key').hide();
+        $('#free').css('background-color', '#45B39D');
+        $('#hide5').show();
+        $('#free').animate({height: '510px'}, 400);
+        $('#free').animate({width: '400px'}, 400);
+        $('#free-text').delay(1100).slideDown();
+    });
+    $('#fusion-key').click(function(){
+        resetAll();
+        $('#fusion-key').hide();
+        $('#fusion').css('background-color', '#DC7633');
+        $('#hide6').show();
+        $('#fusion').animate({height: '510px'}, 400);
+        $('#fusion').animate({width: '400px'}, 400);
+        $('#fusion-text').delay(1100).slideDown();
+    });
+    $('#latin-key').click(function(){
+        resetAll();
+        $('#latin-key').hide();
+        $('#latin').css('background-color', '#58D68D');
+        $('#hide7').show();
+        $('#latin').animate({height: '510px'}, 400);
+        $('#latin').animate({width: '400px'}, 400);
+        $('#latin-text').delay(1100).slideDown();
+    });
+    $('#smooth-key').click(function(){
+        resetAll();
+        $('#smooth-key').hide();
+        $('#smooth').css('background-color', '#FADBD8');
+        $('#hide8').show();
+        $('#smooth').animate({height: '510px'}, 400);
+        $('#smooth').animate({width: '400px'}, 400);
+        $('#smooth-text').delay(1100).slideDown();
+    });
+    $('#acid-key').click(function(){
+        resetAll();
+        $('#acid-key').hide();
+        $('#acid').css('background-color', '#C39BD3');
+        $('#hide9').show();
+        $('#acid').animate({height: '510px'}, 400);
+        $('#acid').animate({width: '400px'}, 400);
+        $('#acid-text').delay(1100).slideDown();
     });
 
-    $('#show4').click(function(){
-        resetAll();
-        $('#div4').css('background-color', '#F4D03F');
-        $('#show4').hide();
-        $('#hide4').show();
-        $('#div4').animate({height: '500px'}, 400);
-        $('#div4').animate({width: '400px'}, 400);
-        $('#text4').delay(1100).slideDown();
-    });
+    $('.album-cover').click(function() {
+
+        hideMusic();
+
+        switch ($(this).attr("id")) {
+          case "original_dixieland-cover":
+            pause();
+            $('#original_dixieland-audio').show();
+            break;
+          case "louis_armstrong-cover":
+            pause();
+            $('#louis_armstrong-audio').show();
+            break;
+          case "benny_goodman-cover":
+            pause();
+            $('#benny_goodman-audio').show();
+            break;
+          case "glenn_miller-cover":
+            pause();
+            $('#glenn_miller-audio').show();
+            break;
+          case "dizzy_gillespie-cover":
+            pause();
+            $('#dizzy_gillespie-audio').show();
+            break;
+          case "art_blakey-cover":
+            pause();
+            $('#art_blakey-audio').show();
+            break;
+          case "miles_davis-cover":
+            pause();
+            $('#miles_davis-audio').show();
+            break;
+          case "chet_baker-cover":
+            pause();
+            $('#chet_baker-audio').show();
+            break;
+          case "john_coltrane-cover":
+            pause();
+            $('#john_coltrane-audio').show();
+            break;
+          case "charles_mingus-cover":
+            pause();
+            $('#charles_mingus-audio').show();
+            break;
+          case "miles_davis2-cover":
+            pause();
+            $('#miles_davis2-audio').show();
+            break;
+          case "weather_report-cover":
+            pause();
+            $('#weather_report-audio').show();
+            break;
+          case "mario_bauza-cover":
+            pause();
+            $('#mario_bauza-audio').show();
+            break;
+          case "stan_getz-cover":
+            pause();
+            $('#stan_getz-audio').show();
+            break;
+          case "grover_washington-cover":
+            pause();
+            $('#grover_washington-audio').show();
+            break;
+          case "kenny_g-cover":
+            pause();
+            $('#kenny_g-audio').show();
+            break;
+          case "jamiroquai-cover":
+            pause();
+            $('#jamiroquai-audio').show();
+            break;
+          case "us3-cover":
+            pause();
+            $('#us3-audio').show();
+            break;
+          }
+      });
+
 
     $('.hide').click(function(){
       resetAll();
+    });
+
+  $(window).resize(function() {
+      location.reload();
     });
 
 
